672d439aac9ccdd034929bdd201476653bee0d3e
karthick <karthickcherukuri@gmail.com>
2023-09-19 20:56:08 +0530

    second commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Courrosal from "./Courrosal";
+import TopTopics from "./TopTopics";
+import Courses from "./Courses";
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Courrosal />
+      <TopTopics />
+      <Courses />
+    </>
+  );
+};
+
+export default Home;
